479a74981de39569d1a451d7bb5101fd2d3c8535
Gabe Scholz <scholz.gabe@gmail.com>
2017-08-28 22:36:49 -0700

    Filter out node_modules and .git

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { readFileSync } from 'graceful-fs';
 import { relative } from 'path';
 
 import { Item } from 'klaw-sync';
-const klawSync: (path: string) => Item[] = require('klaw-sync');
+const klawSync: (
+  path: string,
+  opts: { filter: Function; noRecurseOnFailedFilter: boolean },
+) => Item[] = require('klaw-sync');
 
 export type File = {
   stats: {
@@ -19,8 +22,10 @@ export type DirectorySnapshot = {
   contents: File[];
 };
 
+const isSource = (item: Item) => item.path.indexOf('node_modules') < 0 && item.path.indexOf('.git') < 0;
+
 export const directorySnapshot = (basePath: string): DirectorySnapshot => {
-  const contents = klawSync(basePath).map((item): File => {
+  const contents = klawSync(basePath, { filter: isSource, noRecurseOnFailedFilter: true }).map((item): File => {
     const { stats } = item;
     const path = relative(basePath, item.path);
     const contents = item.stats.isFile() ? readFileSync(item.path).toString() : '';
